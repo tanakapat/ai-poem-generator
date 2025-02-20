30ec237f2e46e5bc9bc087026c5b5429b5433b31
tanakapat <patricia.tanaka.3@hotmail.com>
2025-02-19 22:17:42 -0300

    Added Shecodes AI to generate the poem in swedish

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-function generatePoem(event) {
-    event.preventDefault()
-
+function displayPoem(response)
+ {
     new Typewriter("#poem-section", {
-    strings: "Cute poem here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+ 
+function generatePoem(event) {
+    event.preventDefault();
 
-let poemFormElement = document.querySelector("#poem-generator-form")
-poemFormElement.addEventListener("submit", generatePoem)
+    let instructionsInput = document.querySelector("#user-instructions")
+
+    let apiKey = "bo5101a34b5992391t9e44f085355a8b";
+    let prompt = `Generate a poem about ${instructionsInput}`;
+    let context = "You are a romantic poem expert specialized in create poems with a maximum length of 5 lines in swedish. Please write one phrase for each line and use the basic HTML. Show only the poem text.";
+    apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);    
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
